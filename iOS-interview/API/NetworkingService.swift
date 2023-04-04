497f6e01e5ddece7f689262287e2b46ce84e4fe2
Caleb <129810225+panel-caleb@users.noreply.github.com>
2023-04-04 16:38:13 -0500

    Update NetworkingService (#3)

--- iOS-interview/API/NetworkingService.swift
+++ iOS-interview/API/NetworkingService.swift
@@ -8,18 +8,28 @@
 import Foundation
 
 protocol NetworkingService {
-    func fetchImages<T: Decodable>(offset: Int, NetworkingCompletion: @escaping NetworkingCompletion<T>)
+    /// Fetches Giphy images from local JSON files
+    ///
+    /// For the sake of time, we are providing a static networking service that stubs the giphy API.
+    /// You can find the JSON files in the Resources directory.  We have a max of 100 images available,
+    /// and if you finish the code challenge you could consider writing an actual networking layer
+    /// integrating with Giphy's API.
+    ///
+    /// - Parameters:
+    ///     - page: The page index for pagination.  We have giphy_1, ... , giphy_5.
+    ///     - completion: Swift.Result closure that provides the Decodable data model that you will create for the response or the NetworkingError.
+    func fetchImages<T: Decodable>(page: Int, completion: @escaping NetworkingCompletion<T>)
 }
 
 final class NetworkingServiceImp: NetworkingService {
 
-    func fetchImages<T: Decodable>(offset: Int, NetworkingCompletion: @escaping NetworkingCompletion<T>) {
-        guard (1...5).contains(offset) else {
-            return NetworkingCompletion(.failure( NetworkingError.invalidOffset))
+    func fetchImages<T: Decodable>(page: Int, completion: @escaping NetworkingCompletion<T>) {
+        guard (1..<5).contains(page) else {
+            return completion(.failure( NetworkingError.invalidOffset))
         }
         
-        guard let path: String = Bundle.main.path(forResource: "giphy_\(offset)", ofType: "json") else {
-            return  NetworkingCompletion(.failure(NetworkingError.invalidOffset))
+        guard let path: String = Bundle.main.path(forResource: "giphy_\(page + 1)", ofType: "json") else {
+            return completion(.failure(NetworkingError.invalidOffset))
         }
 
         let url: URL = .init(fileURLWithPath: path)
@@ -28,12 +38,12 @@ final class NetworkingServiceImp: NetworkingService {
             let data: Data = try .init(contentsOf: url)
 
             guard let response = try? JSONDecoder().decode(T.self, from: data) else {
-                return NetworkingCompletion(.failure(NetworkingError.jsonDecoding))
+                return completion(.failure(NetworkingError.jsonDecoding))
             }
 
-            NetworkingCompletion(.success(response))
+            completion(.success(response))
         } catch {
-            NetworkingCompletion(.failure(NetworkingError.invalidJSONFormat))
+            completion(.failure(NetworkingError.invalidJSONFormat))
         }
     }
 }
